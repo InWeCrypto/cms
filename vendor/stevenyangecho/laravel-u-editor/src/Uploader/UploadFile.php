7b32329e7828035f58c39c261506976220bf2b93
huanglin <1968351802@qq.com>
2017-12-02 17:32:44 +0800

    vendor 目录

---
+++ vendor/stevenyangecho/laravel-u-editor/src/Uploader/UploadFile.php
@@ -0,0 +1,89 @@
+<?php 
+namespace Stevenyangecho\UEditor\Uploader;
+
+use Stevenyangecho\UEditor\Uploader\Upload;
+
+/**
+ *
+ *
+ * Class UploadFile
+ *
+ * 文件/图像普通上传
+ *
+ * @package Stevenyangecho\UEditor\Uploader
+ */
+class UploadFile  extends Upload{
+    use UploadQiniu;
+    public function doUpload()
+    {
+
+
+        $file = $this->request->file($this->fileField);
+        if (empty($file)) {
+            $this->stateInfo = $this->getStateInfo("ERROR_FILE_NOT_FOUND");
+            return false;
+        }
+        if (!$file->isValid()) {
+            $this->stateInfo = $this->getStateInfo($file->getError());
+            return false;
+
+        }
+
+        $this->file = $file;
+
+        $this->oriName = $this->file->getClientOriginalName();
+
+        $this->fileSize = $this->file->getSize();
+        $this->fileType = $this->getFileExt();
+
+        $this->fullName = $this->getFullName();
+
+
+        $this->filePath = $this->getFilePath();
+
+        $this->fileName = basename($this->filePath);
+
+
+        //检查文件大小是否超出限制
+        if (!$this->checkSize()) {
+            $this->stateInfo = $this->getStateInfo("ERROR_SIZE_EXCEED");
+            return false;
+        }
+        //检查是否不允许的文件格式
+        if (!$this->checkType()) {
+            $this->stateInfo = $this->getStateInfo("ERROR_TYPE_NOT_ALLOWED");
+            return false;
+        }
+
+        if(config('UEditorUpload.core.mode')=='local'){
+            try {
+                // 本地文件
+                $local_file = $this->file->getPathName();
+                // 线上文件
+                $oss_file   = config('UEditorUpload.core.oss.oss_file_prefix', '/');
+                $oss_file  .= md5_file($local_file) . '.' . $this->file->getClientOriginalExtension();
+
+                $this->fullName = \OSS::updateFile($local_file, $oss_file);
+                // $this->file->move(dirname($this->filePath), $this->fileName);
+                
+                $this->stateInfo = $this->stateMap[0];
+
+            } catch (FileException $exception) {
+                $this->stateInfo = $this->getStateInfo("ERROR_WRITE_CONTENT");
+                return false;
+            }
+
+        }else if(config('UEditorUpload.core.mode')=='qiniu'){
+
+            $content=file_get_contents($this->file->getPathname());
+            return $this->uploadQiniu($this->filePath,$content);
+
+        }else{
+            $this->stateInfo = $this->getStateInfo("ERROR_UNKNOWN_MODE");
+            return false;
+        }
+
+        return true;
+
+    }
+}

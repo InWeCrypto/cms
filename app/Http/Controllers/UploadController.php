5c22ab555a54f73877b84f31e5bd5a949897bcb9
huanglin <1968351802@qq.com>
2017-12-05 15:22:24 +0800

    新增, 上传视频

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -10,7 +10,7 @@ class UploadController extends BaseController
     public function img(Request $request)
     {
         $validator = \Validator::make($request->all(), [
-                'img' => 'required'
+                'img' => 'required|mimes:jpeg,bmp,png'
             ]
         );
         if ($validator->fails()){
@@ -23,7 +23,27 @@ class UploadController extends BaseController
         $extension = $file->getClientOriginalExtension();
         $file_name = $file->getPathName();
         $oss_file  = 'imgs/' . md5_file($file_name) . '.' . $extension;
-        $img       = OSS::updateImg($file_name, $oss_file);
+        $img       = OSS::updateFile($file_name, $oss_file);
         return success(compact('img'));
+    }
+
+    public function video(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+                'video' => 'required|mimes:mp4,3gp,flv'
+            ]
+        );
+        if ($validator->fails()){
+            return fail($validator->messages()->first(), NOT_VALIDATED);
+        }
+        $file = $request->file('video');
+        if(!$file->isValid()){
+            return fail('请选择上传的图片');
+        }
+        $extension = $file->getClientOriginalExtension();
+        $file_name = $file->getPathName();
+        $oss_file  = 'videos/' . md5_file($file_name) . '.' . $extension;
+        $video     = OSS::updateFile($file_name, $oss_file);
+        return success(compact('video'));
     }
 }

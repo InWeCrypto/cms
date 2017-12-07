462bfe9ab82990b4e3f4355012d1c356bede6a00
huanglin <1968351802@qq.com>
2017-12-07 11:24:27 +0800

    修正: jwt错误提示信息

--- app/Http/Middleware/authJWT.php
+++ app/Http/Middleware/authJWT.php
@@ -3,6 +3,10 @@ namespace App\Http\Middleware;
 
 use Tymon\JWTAuth\Facades\JWTAuth;
 
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
 class authJWT
 {
     public function handle($request, \Closure $next)

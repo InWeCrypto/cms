4318e83cc84f62dbdc540cb35e309c50272596cb
what-00 <what-00@qq.com>
2018-03-07 14:48:03 +0800

    纯文本的时候可以不加 desc

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -59,7 +59,7 @@ class ArticleController extends BaseController
             'content' => 'required_unless:type,6,3',
             'url'=> 'required_if:type,6',
             'lang' => 'required',
-            'desc' => 'required',
+            'desc' => 'required_unless:type,1',
         ]);
         if($validator->fails()){
             return fail($validator->errors()->first(), NOT_VALIDATED);

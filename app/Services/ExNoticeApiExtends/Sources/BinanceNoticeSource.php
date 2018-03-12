726a6bb3e62a6e0404d92339f57f213c8f6ec064
what-00 <what-00@qq.com>
2018-03-12 11:35:16 +0800

    测试 交易所公告 币安-最新公告

--- app/Services/ExNoticeApiExtends/Sources/BinanceNoticeSource.php
+++ app/Services/ExNoticeApiExtends/Sources/BinanceNoticeSource.php
@@ -16,6 +16,10 @@ class BinanceNoticeSource extends BaseSource
     protected $lang = '';
 
     protected $ex_notice_name = '';
+    // 下一页
+    protected $next_page_uri = '';
+    // 上一页
+    protected $prev_page_uri = '';
 
     public function lang($val)
     {
@@ -32,13 +36,15 @@ class BinanceNoticeSource extends BaseSource
 
     public function getData($uri='')
     {
-        $return = [];
+        $data = [];
         $uri = $uri ?: $this->uri;
         $article_list = $this->getArticleList($uri);
         foreach($article_list as $li){
-            $return[] = $this->getArticleContent($li);
+            $data[] = $this->getArticleContent($li);
         }
-        return $return;
+        $prev_page = base64_encode($this->prev_page_uri);
+        $next_page = base64_encode($this->next_page_uri);
+        return compact('data','prev_page','next_page');
     }
 
     public function getArticleList($uri)
@@ -47,6 +53,14 @@ class BinanceNoticeSource extends BaseSource
         $html_txt = $this->getHtml($uri);
         $Html = str_get_html($html_txt);
         $Dom = $Html->find('a.article-list-link');
+        // 获取上一页链接
+        if($nextPage = $Html->find('li.pagination-next a', 0)){
+            $this->next_page_uri = $this->uri_prefix . $nextPage->href;
+        }
+        // 获取下一页链接
+        if($prevPage = $Html->find('li.pagination-prev a', 0)){
+            $this->prev_page_uri = $this->uri_prefix . $prevPage->href;
+        }
         foreach($Dom as $li){
             if(empty($li->attr['href'])){
                 continue;

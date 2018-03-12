6e537eb16fde062442d8b8a717faba690c3bf177
what-00 <what-00@qq.com>
2018-03-12 11:48:52 +0800

    测试 交易所公告 币安-最新公告 bug

--- app/Services/ExNoticeApiExtends/Sources/BinanceNoticeSource.php
+++ app/Services/ExNoticeApiExtends/Sources/BinanceNoticeSource.php
@@ -49,6 +49,30 @@ class BinanceNoticeSource extends BaseSource
 
     public function getArticleList($uri)
     {
+        $return = [];
+        $html_txt = $this->getHtml($uri);
+        $Html = str_get_html($html_txt);
+        $Dom = $Html->find('a.article-list-link');
+
+        // 获取上一页链接
+        if($nextPage = $Html->find('li.pagination-next a', 0)){
+            $this->next_page_uri = $this->uri_prefix . $nextPage->href;
+        }
+        // 获取下一页链接
+        if($prevPage = $Html->find('li.pagination-prev a', 0)){
+            $this->prev_page_uri = $this->uri_prefix . $prevPage->href;
+        }
+        foreach($Dom as $li){
+            if(empty($li->attr['href'])){
+                continue;
+            }
+            $return[] = $this->uri_prefix . $li->attr['href'];
+        }
+        return $return;
+    }
+
+    public function getArticleContent($uri)
+    {
         $uri_md5 = md5($uri);
         // 判断 数据库有没有该文章
         if($article = $this->getArticleCache($uri_md5)){
@@ -65,20 +89,6 @@ class BinanceNoticeSource extends BaseSource
         $lang = $this->lang;
         $source = $this->ex_notice_name;
         return $this->setArticleCache($uri, $uri_md5, $source, $lang, $article_title, $article_content, $article_date);
-    }
-
-    public function getArticleContent($uri)
-    {
-        $title = '';
-        $content = '';
-        $html_txt = $this->getHtml($uri);
-        $Html = str_get_html($html_txt);
-        $title = $Html->find('title', 0)->innertext ?: '';
-        $content = $Html->find('div.article-body', 0)->innertext ?: '';
-        $time = $Html->find('li.meta-data', 0)->children(0)->datetime;
-        $lang = $this->lang;
-        $ex_notice_name = $this->ex_notice_name;
-        return compact('title','content','time','uri','lang','ex_notice_name');
     }
 
     public function setArticleCache($uri, $uri_md5, $source, $lang, $article_title, $article_content, $article_date)
